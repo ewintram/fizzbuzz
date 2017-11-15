21e4c2df50d503ce7a929f982f325db2cfa6f8a5
Kesha Kumshayev <kumshkesh@gmail.com>
2017-11-15 16:20:01 +0000

    failing test for 15 (expecting 'fizzbuzz')

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,7 +4,10 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed the number 3' do
     expect(3.fizzbuzz).to eq "fizz"
   end
-  it 'returns "buzz" when passd the number 5' do
+  it 'returns "buzz" when passed the number 5' do
   	expect(5.fizzbuzz).to eq "buzz"
+  end
+  it 'returns "fizzbuzz" when passed the number 15' do
+	expect(15.fizzbuzz).to eq "fizzbuzz"  	
   end
 end
